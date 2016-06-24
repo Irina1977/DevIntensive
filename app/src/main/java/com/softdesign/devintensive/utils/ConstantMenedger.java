10df95fc31181036f801c1f83b774e6aa345f30a
Irina1977 <io77s@yandex.ru>
2016-06-24 20:13:09 +0300

    changes

---
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantMenedger.java
@@ -0,0 +1,9 @@
+package com.softdesign.devintensive.utils;
+
+/**
+ * Created by home on 24.06.2016.
+ */
+public interface ConstantMenedger {
+    String TAG_PREFIX="DEV";
+    String COLOR_MODE_KEY="COLOR_MODE_KEY";
+}
